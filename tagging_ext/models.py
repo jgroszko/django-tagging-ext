9adf7ab6d69b38caedd850cd86a7a8cafbfdf487
John Groszko <grosjoh1@iit.edu>
2010-10-15 21:49:25 -0500

    Update for south 0.7.2

--- tagging_ext/models.py
+++ tagging_ext/models.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.forms import TextInput
 from tagging.fields import TagField
 from tagging_ext.widgets import TagAutoCompleteInput
+
+if "south" in settings.INSTALLED_APPS:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^tagging\_ext\.models\.TagAutocompleteField"])
 
 class TagAutocompleteField(TagField):
     def formfield(self, **kwargs):
